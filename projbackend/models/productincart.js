3201b60529404ec64d91d55ff8bf285c46e5456e
PushpakB3096 <rtpushpak@gmail.com>
2020-04-26 13:02:55 +0530

    Refactor productincart.js
    This is a very minor update in file productincart.js that is
    self-explanatory

--- projbackend/models/productincart.js
+++ projbackend/models/productincart.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
-const {ObjectId} = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
 
 const ProductInCartSchema = new schema({
     product: {
