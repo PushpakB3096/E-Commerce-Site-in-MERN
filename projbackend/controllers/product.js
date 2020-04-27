1ed982fb75a1cb7ce9eae1df9f331505d4d7b693
PushpakB3096 <rtpushpak@gmail.com>
2020-04-27 19:33:30 +0530

    Add more functionality to the product aspect
    This is a major update. Adds -
    1) getAllUniqueCategories functionality.
    2) middlware that updates inventory.
    3) update and delete existing product.
    4) Minor re-formatting of the product route and controller files.
    
    Signed-off-by: PushpakB3096 <rtpushpak@gmail.com>

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -4,81 +4,208 @@ const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
 
-const { validationResult } = require('express-validator');   //importing package for validation
+const { validationResult } = require("express-validator"); //importing package for validation
 
 const Product = require("../models/product");
 
 exports.getProductById = (req, res, next, id) => {
-    Product.findById(id)
-           .populate("category")
-           .exec((err, product) => {
-                if(err || !product){
-                    return res.status(404).json({
-                        error: `Product with ID: ${id} not found`       //TODO: refactor this for proper error message
-                    });
-                }
-                req.product = product;
-                next();
-            });
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(404).json({
+          error: `Product with ID: ${id} not found`, //TODO: refactor this for proper error message
+        });
+      }
+      req.product = product;
+      next();
+    });
 };
 
 exports.createProduct = (req, res) => {
-    const form = new formidable.IncomingForm();
+  const form = new formidable.IncomingForm();
 
-    //configuring the form
-    form.keepExtensions = true;
-    form.maxFileSize = 5 * 1024 * 1024;      //5 mb file size
+  //configuring the form
+  form.keepExtensions = true;
+  form.maxFileSize = 5 * 1024 * 1024; //5 mb file size
 
-    form.parse(req, (err, fields, file) => {
-        if(err){
-            return res.status(400).json({
-                error: "Some problem occured"
-            });
-        }
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Some problem occured",
+      });
+    }
 
-        const errors = validationResult(req);   //getting all the errors from the body of the request, if any
+    const errors = validationResult(req); //getting all the errors from the body of the request, if any
 
-        //checking if there are any validation errors
-        if(!errors.isEmpty()){
-            return res.status(422).json({
-                error: `Param \'${errors.array()[0].param}\' could not be saved. Reason: ${errors.array()[0].msg}`
-            });
-        }
+    //checking if there are any validation errors
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        error: `Param \'${
+          errors.array()[0].param
+        }\' could not be saved. Reason: ${errors.array()[0].msg}`,
+      });
+    }
 
-        const newProduct = new Product(fields);     //creating the new product object
-        
-        //handling file
-        if(file.image){
-            if(file.image.size > (5 * 1024 * 1024)){
-                return res.status(413).json({
-                    error: "The file size is greater than 5 Mb"
-                });
-            }
-            newProduct.image.data = fs.readFileSync(file.image.path);
-            newProduct.image.contentType = file.image.type;
-        }
+    const newProduct = new Product(fields); //creating the new product object
 
-        newProduct.save((err, product) => {
-            if(err || !product){
-                return res.status(400).json({
-                    error: "Some error occured while saving the product"
-                });
-            }
-            res.status(200).json({
-                msg: "Product saved successfully",
-                product: {
-                    name: product.name,
-                    description: product.description,
-                    cost: product.cost,
-                    category: product.category,
-                    stock: product.stock,
-                    sold: product.sold
-                }
-            });
+    //handling file
+    if (file.image) {
+      if (file.image.size > 5 * 1024 * 1024) {
+        return res.status(413).json({
+          error: "The file size is greater than 5 Mb",
         });
-    });
-    
+      }
+      newProduct.image.data = fs.readFileSync(file.image.path);
+      newProduct.image.contentType = file.image.type;
+    }
 
+    newProduct.save((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: "Some error occured while saving the product",
+        });
+      }
+      return res.status(200).json({
+        msg: "Product saved successfully",
+        product: {
+          name: product.name,
+          description: product.description,
+          cost: product.cost,
+          category: product.category,
+          stock: product.stock,
+          sold: product.sold,
+        },
+      });
+    });
+  });
 };
 
+exports.getProduct = (req, res) => {
+  req.product.image = undefined; //not returning the binary data because of performance issues
+  return res.status(200).json(req.product);
+};
+
+//middleware that is used to display image on the product page - separated from the rest of the product data for performance
+exports.image = (req, res, next) => {
+  if (req.product.image.data) {
+    req.set("Content-Type", req.product.iamge.contentType);
+    return res.send(req.product.image.data);
+  }
+  next();
+};
+
+exports.deleteProduct = (req, res) => {
+  const product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err || !deletedProduct) {
+      return res.status(400).json({
+        error: `Failed to delete product with the ID: ${product._id}`,
+      });
+    }
+    return res.status(200).json({
+      msg: "Successfully deleted product",
+    });
+  });
+};
+
+exports.updateProduct = (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  //configuring the form
+  form.keepExtensions = true;
+  form.maxFileSize = 5 * 1024 * 1024; //5 mb file size
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Some problem occured",
+      });
+    }
+
+    const oldProduct = req.product; //fetching the product from req
+    oldProduct = _.extend(oldProduct, fields); //updating the product using lodash module
+
+    //handling file
+    if (file.image) {
+      if (file.image.size > 5 * 1024 * 1024) {
+        return res.status(413).json({
+          error: "The file size is greater than 5 Mb",
+        });
+      }
+      oldProduct.image.data = fs.readFileSync(file.image.path);
+      oldProduct.image.contentType = file.image.type;
+    }
+
+    oldProduct.save((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: "Some error occured while updating the product",
+        });
+      }
+      return res.status(200).json({
+        msg: "Product updated successfully",
+        product: {
+          name: product.name,
+          description: product.description,
+          cost: product.cost,
+          category: product.category,
+          stock: product.stock,
+          sold: product.sold,
+        },
+      });
+    });
+  });
+};
+
+exports.getAllProducts = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 8;
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+
+    Product.find()
+        .select("-image")   //not retrieving the image because of performance
+        .populate("category")
+        .sort([[ sortBy, "asc" ]])
+        .limit(limit)
+        .exec((err, allProducts) => {
+            if (err || !allProducts) {        //TODO: add separate conditions for proper error message
+                return res.status(400).json({
+                    error: "Some error occured while fetching the products",
+                });
+            }
+            return res.status(200).json(allProducts);
+        });
+};
+
+//middleware that updates inventory every time an order is placed successfully
+exports.updateInventory = (req, res, next) => {
+    let operations = req.body.order.products.map(product => {
+        return {
+            updateOne: {
+                filter: { _id: product._id },
+                update: { $inc: { sold: +product.count, stock: -product.count }}
+            }
+        };
+    });
+
+    Product.bulkWrite(operations, {}, (err, products) => {
+        if(err){        //TODO: write proper error message
+            return res.status(400).json({
+                error: "Bulk operations failed"
+            });
+        }
+        next();
+    });
+};
+
+exports.gallAllUniqueCategories = (req, res) => {
+    Product.distinct("category", {}, (err, category) => {
+        if(err){
+            return res.status(400).json({
+                error: "No category found"
+            });
+        }
+        return res.json(category);
+    });
+};
 /* end of product.js controller */
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,28 +1,64 @@
 /* start of product.js route */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { check } = require('express-validator');   //importing package for validation
+const { check } = require("express-validator"); //importing package for validation
 
-const { getProductById, createProduct } = require('../controllers/product');
-const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
-const { getUserById } = require('../controllers/user');
- 
+const {
+  getProductById,
+  createProduct,
+  getProduct,
+  image,
+  updateProduct,
+  getAllProducts,
+  gallAllUniqueCategories
+} = require("../controllers/product");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
+
 //parameters
 router.param("productId", getProductById);
 router.param("userId", getUserById);
 
-//routes
-router.post("/product/create/:userId", 
-// [
-//     //check("name").exists().withMessage("You must provide the name"),
-//     check("description").isLength({ min: 5, max: 500 }).withMessage("Description must be between 5 and 500 characters"),
-//     check("cost").exists().withMessage("You must provide the cost"),
-//     check("category").exists().withMessage("You must provide the category"),
-//     check("stock").exists().withMessage("You must provide the stock of the product")
-// ],
-isSignedIn, isAuthenticated, isAdmin, createProduct);
+//update routes - only used by an admin
+router.post(
+  "/product/create/:userId",
+  //TODO: adding validations here is causing an issue where the validation fails even if it is valid
+  // [
+  //     //check("name").exists().withMessage("You must provide the name"),
+  //     check("description").isLength({ min: 5, max: 500 }).withMessage("Description must be between 5 and 500 characters"),
+  //     check("cost").exists().withMessage("You must provide the cost"),
+  //     check("category").exists().withMessage("You must provide the category"),
+  //     check("stock").exists().withMessage("You must provide the stock of the product")
+  // ],
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
+
+router.delete(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteProduct
+);
+
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+//get routes
+router.get("/product/:productId", getProduct);
+router.get("/product/image/:productId", image); //middleware to diplay iamge of the products
+router.get("/products/", getAllProducts);
+router.get("/product/categories", gallAllUniqueCategories);
 
 module.exports = router;
 
