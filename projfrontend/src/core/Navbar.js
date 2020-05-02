b2ab27c01da3171267a8a1718262ef0d797914f1
PushpakB3096 <rtpushpak@gmail.com>
2020-05-02 14:09:40 +0530

    Add styling to navbar component
    This update focuses on styling the navbar. Please note that, the style
    will definitely going to change in the next updates. Also, the navbar
    component is now being exported in a different way.
    
    Signed-off-by: PushpakB3096 <rtpushpak@gmail.com>

--- projfrontend/src/core/Navbar.js
+++ projfrontend/src/core/Navbar.js
@@ -5,43 +5,62 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-export default function Navbar(){
+const activeTabChecker = (history, path) => {
+    // history is given by react
+    if(history.location.pathname === path){     //if the current path is same as the path provided, change tab link color to white
+        return {
+            color: "#2ECC72"
+        };
+    }
+    return {
+        color: "#FFFFFF"
+    };
+};
+
+const Navbar = ({ history }) => {
     return (
         <div>
             <ul className="nav nav-tabs bg-dark">
                 {/* TODO: add conditional rendering of navbar links */}
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/") }
+                          className="nav-link" to="/">
                         Home
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/cart") }
+                          className="nav-link" to="/cart">
                         Cart
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/user/dashboard") }
+                          className="nav-link" to="/user/dashboard">
                         Dashboard
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/admin/dashboard") }
+                          className="nav-link" to="/admin/dashboard">
                         Admin Dashboard
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/signup") }
+                          className="nav-link" to="/signup">
                         Signup
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/signin") }
+                          className="nav-link" to="/signin">
                         Signin
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                    <Link style={ activeTabChecker(history, "/logout") }
+                          className="nav-link" to="/logout">
                         Logout
                     </Link>
                 </li>
@@ -49,5 +68,7 @@ export default function Navbar(){
         </div>
     );
 };
+
+export default withRouter(Navbar);
 
 /* end of Navbar.js */
