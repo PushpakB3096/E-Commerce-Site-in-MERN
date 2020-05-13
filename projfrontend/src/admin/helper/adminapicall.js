8cd83ba76d20f2eff2801bd60be3c01847b252dd
PushpakB3096 <rtpushpak@gmail.com>
2020-05-13 11:59:00 +0530

    Add Admin API Calls For Product and Category
    Minor change. Adds a few routes related to product and category CRUD
    operations.
    
    Signed-off-by: PushpakB3096 <rtpushpak@gmail.com>

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -1,7 +1,8 @@
 /* start of adminapicall.js */
 import { API } from '../../backend';
 
-export const createCategory = (userId, token, category) => {
+//calls related to category
+export const createCategory = (userId, token, category) => {        //creates a category
     return fetch(`${API}/category/create/${userId}`, {
         method: "POST",
         headers: {
@@ -14,6 +15,81 @@ export const createCategory = (userId, token, category) => {
         return response.json();
     }).catch(err => {
         console.log("An error occured: " +err);
+    });
+};
+
+export const getCategories = () => {        //gets all categories
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    }).then(response => {
+        return response.json();
+    }).catch(err => {
+        return err.json();
+    });
+};
+
+//calls related to product
+export const createProduct = (userId, token, product) => {      //creates a product
+    return fetch(`${API}/product/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    }).then(response => {
+        return response.json();
+    }).catch(err => {
+        return err.json();
+    });
+};
+
+export const getProducts = () => {        //gets all products
+    return fetch(`${API}/products`, {
+        method: "GET"
+    }).then(response => {
+        return response.json();
+    }).catch(err => {
+        return err.json();
+    });
+};
+
+export const getProduct = productId => {        //gets a single product
+    return fetch(`${API}/product/${productId}`, {
+        method: "GET"
+    }).then(response => {
+        return response.json();
+    }).catch(err => {
+        return err.json();
+    });
+};
+
+export const updateProduct = (productId, userId, token, product) => {      //updates a product
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    }).then(response => {
+        return response.json();
+    }).catch(err => {
+        return err.json();
+    });
+};
+
+export const deleteProduct = (productId, userId, token) => {      //deletes a product
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => {
+        return response.json();
+    }).catch(err => {
+        return err.json();
     });
 };
 
