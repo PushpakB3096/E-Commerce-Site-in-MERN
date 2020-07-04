62a83a52a917b4e33cf80a91e75224b1614a6421
PushpakB3096 <rtpushpak@gmail.com>
2020-07-04 19:18:49 +0530

    Deployment change v2

--- client/src/core/helper/coreapicalls.js
+++ client/src/core/helper/coreapicalls.js
@@ -1,13 +1,15 @@
 /* start of coreapicalls.js */
 
-import { API } from '../../backend';
+import { API } from "../../backend";
 
 export const getAllProducts = () => {
-    return fetch(`${API}/products`, {
-        method: "GET"
-    }).then(response => {
-        return response.json();
-    }).catch(err => console.log(err));
+  return fetch(`${API}/products`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
 
 /* end of coreapicalls.js */
